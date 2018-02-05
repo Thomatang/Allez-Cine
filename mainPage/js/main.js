587bbb4df287774e55c077c954db6c557f6541d3
mariomliberio <mariomliberio@gmail.com>
2018-02-05 09:42:44 +0100

    wip

--- mainPage/js/main.js
+++ mainPage/js/main.js
@@ -1,16 +1,14 @@
-// When the user scrolls down 20px from the top of the document, show the button
-/*window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 0) {
-        document.getElementById("arrowBtn").style.display = "block";
-    } else {
-        document.getElementById("arrowBtn").style.display = "none";
-    }
-}
-*/
-// When the user clicks on the button, scroll to the top of the document
+// BUTTON TO TOP FUNCTION
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+// MODAL CONTACT US
+$(document).ready(function(){
+    $("#sendmsg").click(function(){
+        $("#myModal").modal("show");
+    });
+    $("#myModal").on('show.bs.modal', function () {
+
+       });
+});
