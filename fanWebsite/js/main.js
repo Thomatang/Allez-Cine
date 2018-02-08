0a13e2ce1e4763f3612420daa565968e1865e202
Flaviano <flaviano.mucedda@gmail.com>
2018-02-08 09:45:05 +0100

    design DONE

--- fanWebsite/js/main.js
+++ fanWebsite/js/main.js
@@ -1,32 +1,32 @@
+// =======================  NAVBAR SCROLL effect ==================================
+
 $(document).ready(function() {
 
+    $("#newsNav").click(function() {
+
+        $('html, body').animate({
+            scrollTop: $("#news").offset().top - 97.5
+        }, 500);
+    })
+    $("#characterNav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#character").offset().top - 97.5
+        }, 500);
+    })
+    $("#galleryNav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#gallery").offset().top - 97.5
+        }, 500);
+    })
+    $("#shopNav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#shop").offset().top - 97.5
+        }, 500);
+    })
+    $("#contactNav").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top - 97.5
+        }, 500);
+    })
 
 });
-
-// =======================  NAVBAR SCROLL effect ==================================
-$("#newsNav").click(function () {
-    
-    $('html, body').animate({
-        scrollTop: $("#news").offset().top-97.5
-    }, 2000);
-})
-$("#characterNav").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#character").offset().top-97.5
-    }, 2000);
-})
-        $("#galleryNav").click(function () {
-$('html, body').animate({
-    scrollTop: $("#gallery").offset().top - 97.5
-    }, 2000);
-})
-            $("#shopNav").click(function () {
-$('html, body').animate({
-    scrollTop: $("#shop").offset().top - 97.5
-    }, 2000);
-})
-$("#contactNav").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#contact").offset().top - 97.5
-    }, 2000);
-})
